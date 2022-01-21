577f7d0b2a59e079386abcec58fad036a43d0e56
BUYA-GH <baka3737baka@gmail.com>
2022-01-22 01:34:06 +0900

    [feat] add user model and admin page

--- app.js
+++ app.js
@@ -1,37 +1,16 @@
 // app.js
 var express = require('express');
+var app = express();
 const AdminJS = require('adminjs');
-const AdminJSexpress = require('@adminjs/express');
-const models = require("/.models/index.js");
+const AdminJSExpress = require('@adminjs/express');
 
-const app = express();
-
-// admin-js
 const adminJs = new AdminJS({
-    databases: [],
-    rootPath: '/admin',
-});
-const adminRouter = AdminJSexpress.buildRouter(adminJs);
-
-// models
-models.sequelize.sync().then( () => {
-    console.log("DB Connect Success");
-}).catch(err => {
-    console.log("DB Connect Fail");
-    console.log(err);
+  databases: [],
+  rootPath: '/admin',
 });
 
-// for use ejs
-app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
-
-// body-parser
-app.use(adminJs.options.rootPath, adminRouter);
-app.use(express.json());    // body-parser는 express에 포함되었음
-app.use(express.urlencoded({extended:true}));
-
-// Routes
-app.use('/', require('./router'));
+const router = AdminJSExpress.buildRouter(adminJs);
+app.use(adminJs.options.rootPath, router)
 
 // running
 var port = 8000;
