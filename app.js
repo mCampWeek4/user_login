27d80ca00d1700da071bc36f563e4a9db87acc6b
BUYA-GH <baka3737baka@gmail.com>
2022-01-22 15:48:32 +0900

    [feat] something

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
 // app.js
 var express = require('express');
 var app = express();
+
 const AdminJS = require('adminjs');
 const AdminJSExpress = require('@adminjs/express');
+const AdminJSSequelize = require('@adminjs/sequelize')
+
+AdminJS.registerAdapter(AdminJSSequelize);
+
+const models = require('./models/index.js')
+const User = require('./models/User');
 
 const adminJs = new AdminJS({
-  databases: [],
+  databases: [models],
   rootPath: '/admin',
 });
 
-const router = AdminJSExpress.buildRouter(adminJs);
+//const router = AdminJSExpress.buildRouter(adminJs);
+const router = AdminJSExpress.buildAuthenticatedRouter(adminJs, {
+    authenticate: async(userid, password) => {
+        if(User.password === password && User.userid === userid && User.isAdmin === 1) {
+            return User;
+        }
+        return null
+    }
+});
+
 app.use(adminJs.options.rootPath, router)
+
+models.sequelize.sync().then( () => {
+    console.log("DB Connect Success!");
+}).catch((err) => {
+    console.log("DB Connect Fail!");
+    console.log(err);
+});
 
 // running
 var port = 8000;
---
+++ models/User.js
@@ -0,0 +1,80 @@
+// models/User.js
+var crypto = require('crypto');
+
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define("Uses", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true,
+            required: true
+        },
+        userid: {
+            type: DataTypes.STRING(21),
+            allowNull: false,
+            required: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            required: true,
+            get() {
+                return () => this.getDataValue('password')
+            }
+        },
+        salt: {
+            type: DataTypes.STRING,
+            get() {
+                return () => this.getDataValue('salt')
+            }
+        },
+        isAdmin: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            required: true
+        }
+    }, {
+            underscored: true,
+            freezeTableName: true,
+            tableName: "user"
+    });
+
+    // hashing password
+    const createSalt = () =>
+        new Promise((resolve, reject) => {
+            crypto.randomBytes(64, (err, buf) => {
+                if (err) reject(err);
+                resolve(buf.toString('base64'));
+            });
+        });
+
+    const createHashedPassword = (plainPassword) =>
+        new Promise(async (resolve, reject) => {
+            const salt = await createSalt();
+            crypto.pbkdf2(plainPassword, salt, 72277, 64, 'sha512', (err, key) => {
+                if (err) reject(err);
+                resolve({ password: key.toString('base64'), salt });
+            });
+        });
+
+    const compareHashPassword = function (curPassword, oriPassword, salt) {
+        crypto.pbkdf2(curPassword, salt, 72277, 64, 'sha512', (err, key) => {
+            if (err) reject(err);
+            return (key.toString('base64') == oriPassword);
+        });
+    }
+
+    const setSaltAndPassword = async function(user) {
+        if(user.changed('password')) {
+            const {password, salt} = await createHashedPassword(user.password());
+            user.password = password;
+            user.salt = salt;
+        }
+    }
+
+    User.beforeCreate(setSaltAndPassword);
+    User.beforeUpdate(setSaltAndPassword);
+
+    return User;
+};
